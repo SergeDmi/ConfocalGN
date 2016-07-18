e0bf985535d4d7fe8636d1b3289733bb14a28cea
SergeDmi <serge.dmitrieff@gmail.com>
2016-07-18 10:00:28 +0200

    cleaned example

--- mock_example.m
+++ mock_example.m
@@ -1,18 +1,9 @@
 % Example of mock confocal data generation
 %   We first generate a baseball seam curve
 %   We then make it confocal
+%   We then segment it
 
-%% Parameters
-% Degree of coiling of the base ball seam curve
-b=0.02;
-% Orientation of the BBseam curve
-angs=[pi/6 pi/6 0];
-% Radius of the sphere containing BB seam curve
-R=90;
-% Resolution 
-dt=0.001;
-% Size of the "perfect" image
-Npts=300;
+%% CONFOCAL GN Parameters
 % Properties of the microscope
 psf=[8 8 32];
 pix=[6 6 24];
@@ -27,7 +18,19 @@ mean_bg=0;
 stdev_bg=0.02;
 % pixel noise from camera
 mean_px=0;
-stdev_px=0.003;
+stdev_px=0.00001;
+
+%% PARAMETER for ground truth generation
+% Degree of coiling of the base ball seam curve
+b=0.02;
+% Orientation of the BBseam curve
+angs=[pi/6 pi/6 0];
+% Radius of the sphere containing BB seam curve
+R=90;
+% Resolution 
+dt=0.001;
+% Size of the ground truth in discretized units
+Npts=[300 300 300];
 
 %% Generating a set of points ; here, a BBseam curve
 RR=generate_bbseam(Npts,b,R,dt,angs);
@@ -41,10 +44,13 @@ RR=generate_bbseam(Npts,b,R,dt,angs);
 [stacks,offset]=generate_stacks(img,psf,pix,mean_px,stdev_px);
 
 %% Image segmentation to compare
-im=threshold(stacks);
+% we blur the stack with a gaussian kernel and segment it
+filt=[1 1 0];
+st2=gauss3filter(stacks,filt);
+im=threshold(st2);
+% We then convert from stack to pixel coordinatees Pts and intensities W
 [ Pts,W ] = convertpoints( im , pix );
-Pts(1:2,:)=Pts(1:2,:)-offset(1:2)'*ones(1,size(Pts,2))/2;
-Pts(3,:)=Pts(3,:)-offset(3);
+Pts(1:3,:)=Pts(1:3,:)-offset(1:3)'*ones(1,size(Pts,2));
 
 %% Plotting 
 if 1
