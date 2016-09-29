3030889590ddc39b4402af42d0423059eff9f99e
SergeDmi <serge.dmitrieff@gmail.com>
2016-09-29 16:10:26 +0300

    better labeling

--- segmentation/convertpoints.m
+++ segmentation/convertpoints.m
@@ -1,6 +1,8 @@
 function [ Pts,W ] = convertpoints( img , pixsizes,offset)
-% Convert a 1D to 3D stack into a cloud of weighted points
-%   Pixsizes correspont to the pixel size along each dimension
+% Convert a 1D/2D/3D stack into an array of points coordinates Pts with
+% weights W
+%   Pixsizes corresponds to the pixel size along each dimension
+%   Offset is the an offset 
 %  
 % Serge Dmitrieff, EMBL, 2015
 if nargin<3
@@ -13,22 +15,26 @@ s=size(img);
 l=length(pixsizes);
 
 if length(s)==l && length(s)<4
-    %ix=(1:numel(img));
-    %ixes=ix(~isnan(img(:)))';
    	ixes=logical(~isnan(img(:)))';
-    %ixes=ix(ixes);
 	if l==1
+        % 1D
 		X=pixsizes(1)*(1:s(1));
-		%np=sum(ixes);
 		Pts=X(ixes);
 		W=img(ixes);
     else
 		XX=pixsizes(1)*((1:s(1))')*ones(1,s(2));
 		YX=pixsizes(2)*ones(s(1),1)*(1:s(2));
 		if l==2
+            % 2D
 			Pts=[XX(ixes);YX(ixes)];
 			W=img(ixes);
-		else
+        else
+            % 3D
+            % This is the fastest I found : generate arrays X Y Z
+            % X Y Z have a the size of the stack
+            % X(n) corresponds to the X coordinate of the point n in the
+            % stack 
+            % Please let me know if there is faster !
 			X=repmat(XX,[1,1,s(3)]);
 			Y=repmat(YX,[1,1,s(3)]);
 			ZX=pixsizes(3)*ones(s(2),1)*(1:s(3));
@@ -38,13 +44,13 @@ if length(s)==l && length(s)<4
 		end
 	end
 else
-	Pts=[];
-	W=[];
+    if length(s)~=l
+        error('Pix size dimension not matching img dimension')
+    end
+	error('Too many dimensions')
 end
-
 n=length(W);
 Pts=Pts+(ones(n,1)*offset)';
-
 
 end
 
