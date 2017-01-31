ae0d1e02fda23de85ac64cb4c2bfc8cd7d8e15e1
SergeDmi <serge.dmitrieff@gmail.com>
2017-01-31 19:21:11 +0100

    "updated for better use"

--- segmentation/segment_image.m
+++ segmentation/segment_image.m
@@ -1,19 +1,35 @@
-function [ img,mask] = segment_image(image,filt,ix)
+function [ img,mask] = segment_image(image,options)
 % Minimal image segmentation function
 %   Gaussian blurring + thresholding
-% 
-% Should be replaced by your own !
+%   Should be replaced by your own !
+%
+%   img is the thresholded image
+%   mask is the mask to apply to the image to obtain the background
+%
+% options or options.segmentation should contain filt
+% filt is a 3x1 vector for the Gaussian blurring to be applied
 %
 % Serge Dmitrieff, Nédélec Lab, EMBL 2016
 % www.biophysics.fr
 
-if nargin<3
-    ix=[];
-    if nargin<2
-        warning('Default : no filer')
-        filt=[0 0 0];
-    end     
+defopt=cgn_options_load();
+if nargin<2
+    options=defopt;
 end
+if isfield(options,'segmentation')
+    options=options.segmentation;
+end
+if isfield(options,'filt')
+    filt=options.filt;
+else
+    filt=defopt.segmentation.filt;
+end
+if isfield(options,'ix')
+    ix=options.ix;
+else
+    ix=defopt.segmentation.ix;
+end
+
 if ischar(image)
     if ~isempty(ix)
         imgs=tiffread(image,ix);
