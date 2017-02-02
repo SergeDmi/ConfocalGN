73cbfd13c880b03b5d8d2fb3e6ca4e8476825741
SergeDmi <serge.dmitrieff@gmail.com>
2017-02-02 20:28:55 +0100

    "renaming of options"

--- cgn_options.m
+++ cgn_options.m
@@ -10,8 +10,8 @@ function opt = confocal_options
 opt.segmentation.filt             = [1 1 0];
 opt.segmentation.ix               = [];
 % Options for sample segmentation
-opt.sample.filt                   = [1 1 0];
-opt.sample.ix                     = 9:16;
+opt.sampling.filt                   = [1 1 0];
+opt.sampling.ix                     = 9:16;
 % Misc
 opt.verbose                       = 1;
 return
