663c51d7c3c70ee74a9313f936f54a4bee83c0aa
SergeDmi <serge.dmitrieff@gmail.com>
2017-02-03 09:06:37 +0100

    added plotting tools

---
+++ cgn/plot_simul_results.m
@@ -0,0 +1,39 @@
+function [  ] = plot_simul_results(truth,im,conf,offset,options)
+% plot results of simulated stacks
+%   compares ground truth properties to simulated
+
+defopt=cgn_options_load();
+if nargin<5
+    options=defopt;
+    if nargin<1
+        error('Error : get_img_params requires an image or or image filename')
+    end
+end
+if isfield(options,'segmentation')
+    options=options.segmentation;
+end
+
+
+ % Convert from stack to pixel coordinatees Pts and intensities W
+% (because of smaller scale, there is an offset between the stack and
+% the ground truth) 
+Pts  = convertpoints(im, conf.pix, offset);
+
+% Converting ground truth image to points
+img2=segment_image(truth,options);
+pts = convertpoints(img2);
+
+% convert the coordinates to voxel units
+Pts(:)=Pts(:)/conf.pix(1);
+pts(:)=pts(:)/conf.pix(1);
+
+% Plot data points:
+figure
+hold all
+scatter3(pts(1,:),pts(2,:),pts(3,:),'k.')
+scatter3(Pts(1,:),Pts(2,:),Pts(3,:),'b')
+axis equal
+
+
+end
+
