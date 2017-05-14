4ee975547f41d3cfe59eb082636e0ebd1074b484
Serge Dmitrieff <serge.dmitrieff@gmail.com>
2017-05-14 15:44:22 +0200

    CGN update : now loading ground_truth from florophore positions

---
+++ gtm_options.m
@@ -0,0 +1,30 @@
+function opt = gtm_options
+% Provide the options to analyse
+%
+% They are loaded by gtm_option_load.m,
+% which also check for a local 'gtm_options.m'
+%
+%
+
+%% Options for the output image
+opt.image_size            = [512 512 162];
+opt.offset                =  [];
+opt.scaling               =  [];
+opt.rotation              =  [0 0 0];
+
+%% Options for the signal
+opt.signal                  = 100 ;
+opt.fluorophore                  = 'poisson';
+%opt.fluorophore                  = 'permanent';
+
+%% Options for the background
+% moments of the background fluorescence distribution
+%opt.background                  = [1 1 1] ;
+opt.background            = [10 1 1] ;
+
+
+%% Misc
+% moments of the background fluorescence distribution
+opt.verbose               = 1 ;
+
+return
