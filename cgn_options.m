d2de975fa696bcffbfe0649f0ac1e5e57b229473
Serge Dmitrieff <serge.dmitrieff@gmail.com>
2017-05-31 21:53:44 +0200

    improved comments

--- cgn_options.m
+++ cgn_options.m
@@ -6,15 +6,19 @@ function opt = confocal_options
 %
 %
 
-% Options for image segmentation
+%% Options for image segmentation
+% Filter to be used before thresholding
 opt.segmentation.filt             = [1 1 0];
+% Indexes of items to be kept when loading the stack
 opt.segmentation.ix               = [];
 
-% Options for sample segmentation
+%% Options for sample segmentation
+% Filter to be used before thresholding
 opt.sampling.filt                 = [1 1 0];
+% Indexes of items to be kept when loading the stack
 opt.sampling.ix                   = 9:16;
 
-% Misc
+%% Misc
 opt.verbose                       = 1;
 
 return
