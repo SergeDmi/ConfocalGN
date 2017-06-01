0fadabe0d3d923568124a20830136e15a807c432
Serge Dmitrieff <serge.dmitrieff@gmail.com>
2017-06-01 17:46:31 +0200

    better commenting

--- readfiles/txt_options.m
+++ readfiles/txt_options.m
@@ -1,19 +1,20 @@
 function [ opt ] = txt_options(  )
 % Options for text parsing
 %% File structures
+% Delimiter between coordinates
 opt.delimiter=' ';
-opt.maxim=3;
 
 %% Use comments for text files
 % Lines starting with a comment are discarded
 opt.comments={'%','#','fiber','frame','Volume','Set'};
 
-%% Use indicators for .obj files
+%% Use indicators for lines to keep
 % Lines not starting by an indicator are discarded
-% Warning : using indicators overrides oppt.comments
+% Warning : using indicators overrides opt.comments
+% E.g. for and .obj file
 % opt.indicators={'v','vn','vt'};
 
-%% Range of points to take
+%% Range of columns to take
 opt.range=3:5;
 end
 
